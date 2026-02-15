ce5bf1ba095bd9c7ac93d70c76e79cfba36f92b5
anjalipandeyncs-spec <anjalipandey.ncs@gmail.com>
2026-02-15 15:20:06 +0530

    app.py

--- app.py
+++ app.py
@@ -18,7 +18,21 @@ if uploaded_file:
     scaler = joblib.load("model/scaler.pkl")
     model = joblib.load(f"model/{model_name}.pkl")
 
+    # Get training feature names
+    train_cols = model.feature_names_in_
+
+    # Add missing columns
+    for col in train_cols:
+        if col not in data.columns:
+            data[col] = 0
+
+    # Remove extra columns and align order
+    data = data[train_cols]
+
+    # Scale
     data_scaled = scaler.transform(data)
+
+    # Predict
     predictions = model.predict(data_scaled)
 
     st.write("Predictions:")
